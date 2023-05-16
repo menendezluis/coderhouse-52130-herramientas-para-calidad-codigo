7e6cb88ca1e677a78c0ab5111bdb2410f4f8ba35
“Luis <lmenendez.1@gmail.com>
2023-05-16 14:09:26 -0600

    Clase 1: SQL

--- clase1/clase1.sql
+++ clase1/clase1.sql
@@ -1,13 +1,61 @@
 show databases;
-drop database ecommerce;
 create database ecommerce;
+create database otrabasededatos;
 use ecommerce;
+drop database ecommerce;
+show tables;
+select curdate() from dual;
+select 1 from dual;
+
+
+select * from clientes
+
 CREATE TABLE clientes (
   client_id INT PRIMARY KEY,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   email VARCHAR(100)
 );
+/*asignando llave primaria desde el create table */
+DROP TABLE USER;
+CREATE TABLE USER
+(
+    id_user    INT         PRIMARY KEY auto_increment,
+    first_name VARCHAR(30) not null,
+    last_name  VARCHAR(30) not null,
+    birthday   DATE        not null,
+    e_mail     varchar(30) not null,
+    nick       varchar(50),
+    password   varchar(50) not null
+);
+select * from USER;
+
+/*asignando llave primaria desde el alter table */
+DROP TABLE USER;
+CREATE TABLE USER
+(
+            id_user    INT,
+    first_name VARCHAR(30) not null,
+    last_name  VARCHAR(30) not null,
+    birthday   DATE        not null,
+    e_mail     varchar(30) not null,
+    nick       varchar(50),
+    password   varchar(50) not null
+
+);
+select * from USER;
+alter table USER
+MODIFY id_user INT PRIMARY KEY  auto_increment;
+select * from USER;
+
+alter table USER
+MODIFY e_mail VARCHAR(25) not null;
+
+alter table USER
+ADD dni VARCHAR(25) not null;
+
+
+
 
 CREATE TABLE producto (
   product_id INT PRIMARY KEY,
@@ -49,6 +97,33 @@ VALUES
     (15, 'Javier', 'Moreno', 'javier@correo.com'),
   (16, 'Laura', 'Torres', 'laura@example.com')
 
+select * from clientes
+
+/*update & delete son un select vitaminado */
+update user set first_name ='Carlos'
+where id_user >= 3
+and  first_name = "Luis"
+update  user set first_name = 'Juan'
+where id_user >= 3
+and  first_name = 'Luis'
+select *from user
+where id_user= 7
+
+delete from USER
+where id_user= 7
+
+select  first_name, last_name, count(*) from user
+group by first_name, last_name
+order by 3
+      /* ,1
+ASC por Count
+AsC first_name
+*/
+
+
+
+insert into user
+value (null, 'Juan', 'Pérez','2001-03-07 00:00:00','juan@correo.com','juanito','123')
 INSERT INTO producto (product_id, product_name)
 VALUES
   (1, 'iPad'),
@@ -63,5 +138,4 @@ VALUES
   (10, 'Nintendo Switch')
 
 /*INSERT INTO venta (sale_id, client_id, date)*/
-
 
